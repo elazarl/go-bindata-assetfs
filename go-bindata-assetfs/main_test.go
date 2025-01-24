b1ed96d13615a2ce3f36156f18ef76059d2be83b
Matilda Horger <matilda.horger@freshrealm.com>
2025-01-24 10:46:49 -0800

    Make tests more concise

--- go-bindata-assetfs/main_test.go
+++ go-bindata-assetfs/main_test.go
@@ -3,108 +3,85 @@ package main
 import "testing"
 import "regexp"
 import "os"
+import "reflect"
 
 var temp_pattern *regexp.Regexp = regexp.MustCompile(`^/tmp/`)
 var exec_pattern *regexp.Regexp = regexp.MustCompile(`go-bindata`)
 
-func TestConfigParseEmpty(t *testing.T) {
-	c, err := parseConfig([]string{})
+func helper(t *testing.T, tmp string, args []string) Config {
+	c, err := parseConfig(args)
 	if err != nil {
 		t.Fatalf("parseConfig failed: %v", err)
 	}
 
-	if !temp_pattern.MatchString(c.TempPath) {
+	if tmp != "" {
+		if c.TempPath != tmp {
+			t.Fatalf("Expected c.TempFile to be %s, got %s", tmp, c.TempPath)
+		}
+	} else if temp_pattern.MatchString(c.TempPath) {
+		os.Remove(c.TempPath)
+	} else {
 		t.Fatalf("TempPath should live in system temp directory, got %s", c.TempPath)
 	}
-	defer os.Remove(c.TempPath)
 
-	if c.Debug != false {
-		t.Fatalf("Debug should not default to true")
-	}
 	if !exec_pattern.MatchString(c.ExecPath) {
 		t.Fatalf("ExecPath should point to go-bindata binary, got %s", c.ExecPath)
 	}
-	if c.OutPath != "bindata.go" {
-		t.Fatalf("OutPath should default to bindata.go, got %s", c.OutPath)
+	return c
+}
+
+func TestConfigParseEmpty(t *testing.T) {
+	c := helper(t, "", []string{})
+	expected := Config {
+		Debug: false,
+		ExecPath: c.ExecPath,
+		TempPath: c.TempPath,
+		OutPath: "bindata.go",
+		Args: []string{},
 	}
-	if len(c.Args) != 0 {
-		t.Fatalf("Args should be empty, got %v", c.Args)
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatalf("Expected %v, got %v", expected, c)
 	}
 }
 
 func TestConfigParseDebug(t *testing.T) {
-	c, err := parseConfig([]string{"-debug"})
-	if err != nil {
-		t.Fatalf("parseConfig failed: %v", err)
+	c := helper(t, "", []string{"-debug", "-debug", "-debug"})
+	expected := Config {
+		Debug: true,
+		ExecPath: c.ExecPath,
+		TempPath: c.TempPath,
+		OutPath: "bindata.go",
+		Args: []string{},
 	}
-
-	if !temp_pattern.MatchString(c.TempPath) {
-		t.Fatalf("TempPath should live in system temp directory, got %s", c.TempPath)
-	}
-	defer os.Remove(c.TempPath)
-
-	if c.Debug != true {
-		t.Fatalf("Debug was requested")
-	}
-	if !exec_pattern.MatchString(c.ExecPath) {
-		t.Fatalf("ExecPath should point to go-bindata binary, got %s", c.ExecPath)
-	}
-	if c.OutPath != "bindata.go" {
-		t.Fatalf("OutPath should default to bindata.go, got %s", c.OutPath)
-	}
-	if len(c.Args) != 0 {
-		t.Fatalf("Args should be empty, got %v", c.Args)
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatalf("Expected %v, got %v", expected, c)
 	}
 }
 
 func TestConfigParseArgs(t *testing.T) {
-	c, err := parseConfig([]string{"x", "y", "-debug", "z"})
-	if err != nil {
-		t.Fatalf("parseConfig failed: %v", err)
+	c := helper(t, "", []string{"x", "y", "-debug", "z"})
+	expected := Config {
+		Debug: true,
+		ExecPath: c.ExecPath,
+		TempPath: c.TempPath,
+		OutPath: "bindata.go",
+		Args: []string{"x", "y", "z"},
 	}
-
-	if !temp_pattern.MatchString(c.TempPath) {
-		t.Fatalf("TempPath should live in system temp directory, got %s", c.TempPath)
-	}
-	defer os.Remove(c.TempPath)
-
-	if c.Debug != true {
-		t.Fatalf("Debug was requested")
-	}
-	if !exec_pattern.MatchString(c.ExecPath) {
-		t.Fatalf("ExecPath should point to go-bindata binary, got %s", c.ExecPath)
-	}
-	if c.OutPath != "bindata.go" {
-		t.Fatalf("OutPath should default to bindata.go, got %s", c.OutPath)
-	}
-	if len(c.Args) != 3 || c.Args[0] != "x" || c.Args[1] != "y" || c.Args[2] != "z" {
-		t.Fatalf("Args should be [x,y,z], got %v", c.Args)
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatalf("Expected %v, got %v", expected, c)
 	}
 }
 
 func TestConfigParsePaths(t *testing.T) {
-	c, err := parseConfig([]string{"-t", "tempfile.go", "-o", "outfile.go"})
-	if err != nil {
-		t.Fatalf("parseConfig failed: %v", err)
+	c := helper(t, "tempfile.go", []string{"-t", "tempfile.go", "-o", "outfile.go"})
+	expected := Config {
+		Debug: false,
+		ExecPath: c.ExecPath,
+		TempPath: "tempfile.go",
+		OutPath: "outfile.go",
+		Args: []string{},
 	}
-
-	if c.TempPath != "tempfile.go" {
-		t.Fatalf("TempPath should be tempfile.go, got %s", c.TempPath)
-		if temp_pattern.MatchString(c.TempPath) {
-			os.Remove(c.TempPath)
-		}
-	}
-
-	if c.Debug != false {
-		t.Fatalf("Debug was not requested")
-	}
-	if !exec_pattern.MatchString(c.ExecPath) {
-		t.Fatalf("ExecPath should point to go-bindata binary, got %s", c.ExecPath)
-	}
-	if c.OutPath != "outfile.go" {
-		t.Fatalf("OutPath should be outfile.go, got %s", c.OutPath)
-	}
-	if len(c.Args) != 0 {
-		t.Fatalf("Args should be empty, got %v", c.Args)
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatalf("Expected %v, got %v", expected, c)
 	}
 }
